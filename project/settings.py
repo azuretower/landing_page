ce484747465fa64b1650b1fc11f80673b233eae0
Ryan <ryan.merrill215@gmail.com>
2015-09-08 12:17:29 -0600

    setup stuff

--- project/settings.py
+++ project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#epjze*1(uzt32p=(l2ns6m7k5bd4ezh+o*e$3m$zs!0ac-i@k'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -74,13 +71,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization
