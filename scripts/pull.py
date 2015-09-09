a04a2499bedb0b588a45b10e965f415960f7ae5c
Ryan <ryan.merrill215@gmail.com>
2015-09-09 14:05:17 -0600

    new errors

--- scripts/pull.py
+++ scripts/pull.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def update():
-    p = subprocess.Popen(['git', 'pull'], cwd='/sites/projects/landing_page/' stdout=subprocess.PIPE, 
+    p = subprocess.Popen(['git', 'pull'], cwd='/sites/projects/landing_page/', stdout=subprocess.PIPE, 
                                         stderr=subprocess.PIPE)
     out, err = p.communicate() 
     return out, err
