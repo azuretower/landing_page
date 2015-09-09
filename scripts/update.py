7846a7fae6f66087a1b742d03fe36cfd6a81efa0
Ryan <ryan.merrill215@gmail.com>
2015-09-09 14:38:57 -0600

    new

--- scripts/update.py
+++ scripts/update.py
@@ -4,9 +4,19 @@ import subprocess
 
 
 def pull():
-    p = subprocess.Popen(['git', 'pull'],
+    pull = subprocess.Popen(['git', 'pull'],
                         cwd='/sites/projects/landing_page/',
                         stdout=subprocess.PIPE, 
                         stderr=subprocess.PIPE)
-    out, err = p.communicate() 
+
+    out, err = pull.communicate()
+    return out, err
+
+def collect():
+    collect = subprocess.Popen(['./manage.py', 'collectstatic'],
+                        cwd='/sites/projects/landing_page/',
+                        stdout=subprocess.PIPE, 
+                        stderr=subprocess.PIPE)
+
+    out, err = collect.commuincate()
     return out, err
