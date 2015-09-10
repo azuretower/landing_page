f8dbbea605b4c566b8eb33e524d1383565950b1f
Ryan <ryan.merrill215@gmail.com>
2015-09-10 10:56:21 -0600

    adjusted navbar

--- github_webhooks/views.py
+++ github_webhooks/views.py
@@ -9,13 +9,17 @@ def update_view(request):
     # http://eli.thegreenplace.net/2014/07/09/payload-server-in-python-3-for-github-webhooks
     if request.method == 'POST' or 'GET':
         context = {}
+
         pull_response, pull_error = update.pull()
-        collect_response, collect_error = update.collect()
-        restart_response, restart_error = update.restart()
         context['pull'] = pull_response
         context['pull_error'] = pull_error
+
+        collect_response, collect_error = update.collect()
         context['collect'] = collect_response
         context['collect_error'] = collect_error
+        
+        restart_response, restart_error = update.restart()
         context['restart'] = restart_response
         context['restart_error'] = restart_error
+
         return render(request, 'response.html', context)
